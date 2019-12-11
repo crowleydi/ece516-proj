19f5f80c78a360196f12cee9a33ab2c5f4cf9737
David Crowley <crowleyd@unm.edu>
2019-12-11 16:35:19 -0700

    optimizing a svr, plots

--- graddesc.py
+++ graddesc.py
@@ -7,7 +7,7 @@ def _norm(X):
 	return math.sqrt(total)
 
 def gradapprox(f, fx, x, dx):
-	g = x * 0.0
+	g = x*0.0
 	ddx = dx*0.0
 	for i in range(len(x)):
 		ddx[i] = dx[i]
@@ -29,10 +29,13 @@ def graddesc(f, x0, dx, T=30, alpha=1, gtol=1e-2, tol=1e-6, obs=None):
 
 		x1 = x+alpha*g
 		fx1 = f(x1)
+		diff = abs(fx-fx1)
 
 		nstep = nstep + 1
 
-		while (fx1 > fx):
+		loop = 4
+		while (fx1 > fx*1.1 and loop > 0):
+			loop = loop - 1
 			alpha = alpha / 2.0
 			nstep = 0
 			x1 = x+alpha*g
@@ -53,10 +56,12 @@ def graddesc(f, x0, dx, T=30, alpha=1, gtol=1e-2, tol=1e-6, obs=None):
 			nstep = 0
 
 		if (diff < tol):
+			print("exiting tolerance")
 			break
 
-		if(gnorm < gtol):
-			break
+		#if(gnorm < gtol):
+			#print("exiting gradient")
+			#break
 
 	if (obs != None):
 		obs(fx, x, g, alpha)
---
+++ svr.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from graddesc import graddesc
+
+# set to True for SVR
+# set to False for NuSVR
+svr=True
+
+# read the data file
+df = pd.read_csv("data.csv", header=None)
+
+# break the data file up into components as numpy arrays
+#
+#ftest = df.iloc[:,0].to_numpy()
+#ftrain = df.iloc[:,1].to_numpy()
+Xtest = df.iloc[:,2:21].to_numpy()
+Xtrain = df.iloc[:,21:40].to_numpy()
+ytest = df.iloc[:,40].to_numpy()
+ytrain = df.iloc[:,41].to_numpy()
+
+fevals = 0;
+def loss_func(x):
+	global svr, Xtrain, ytrain, fevals
+	fevals = fevals + 1
+	C = 10**x[0]
+	eps = 10**x[1]
+	nu = x[1]
+	
+	if nu < 0.1:
+		nu = 0.1
+	elif nu > 0.9:
+		nu = 0.9
+
+	if svr:
+		clf = svm.SVR(gamma='auto',C=C,epsilon=eps)
+	else:
+		clf = svm.NuSVR(gamma='auto',C=C,nu=nu)
+	
+	scores = cross_val_score(clf, Xtrain, ytrain, cv=5,scoring='neg_mean_squared_error')
+	print("scores="+str(scores))
+	return -10*scores.mean()
+
+
+#
+# Generate a 3D surface plot of the loss_func function
+#
+X = np.arange(-1, 2, 0.1)
+if svr:
+	Y = np.arange(-3, -1, 2/40)
+else:
+	Y = np.arange(0.1,0.9,0.8/40)
+
+X, Y = np.meshgrid(X, Y)
+Z = X * 0.0
+
+for i in range(X.shape[0]):
+	for j in range(X.shape[1]):
+		Z[i,j] = loss_func([X[i,j],Y[i,j]])
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=plt.cm.hot, alpha=0.7)
+# linewidth=0, antialiased=False)
+
+# function to observe the gradient descent
+X = []
+Y = []
+Z = []
+
+def myobs(fx, x, g, alpha):
+	global X, Y, Z
+	X.append(x[0])
+	Y.append(x[1])
+	Z.append(fx)
+
+# calculate a random starting point
+C = np.random.uniform(low=-1,high=2)
+if svr:
+	eps = np.random.uniform(low=-3,high=-1)
+	x0=np.array([C, eps])
+else:
+	nu = np.random.uniform(low=0.1,high=0.9)
+	x0=np.array([C, nu])
+
+dx = np.array([0.01,0.01])
+
+#print("loss_func(x0)="+str(loss_func(x0)))
+#print("loss_func(x0)="+str(loss_func(x0)))
+fevals = 0
+x = graddesc(loss_func,x0,dx,obs=myobs,alpha=0.25)
+print("final solution="+str(x))
+print("function evals="+str(fevals))
+ax.plot(X,Y,Z,'go-')
+plt.show()
